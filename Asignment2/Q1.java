524e0bad4910c35a78dc8d7d0343c750de374929
arzoo19goyal <56269320+arzoo19goyal@users.noreply.github.com>
2020-08-31 19:58:10 +0530

    Update Q1.java

--- Asignment2/Q1.java
+++ Asignment2/Q1.java
@@ -6,29 +6,29 @@ public class Main {
         Scanner obj=new Scanner (System.in);
         String string1=obj.nextLine();
         String string2=obj.nextLine();
-        int i1=0;
+        int i=0;
         int flag=0;
-        while(i1<string1.length() && i1<string2.length())
+        while(i<string1.length() && i<string2.length())
         {
-            if(string1.charAt(i1) < string2.charAt(i1)) {
+            if(string1.charAt(i) < string2.charAt(i)) {
                 System.out.println(string1 + " is smaller than " + string2 + " lexicographically");
                 flag=1;
                 break;
             }
-            else if(string1.charAt(i1) > string2.charAt(i1)) {
+            else if(string1.charAt(i) > string2.charAt(i)) {
                 System.out.println(string2 + " is smaller than " + string1 + " lexicographically");
                 flag=1;
                 break;
             }
             else {
-                i1++;
+                i++;
             }
         }
         if(flag==0)
         {
-            if(i1!=string1.length() && i1==string2.length())
+            if(i!=string1.length() && i==string2.length())
                 System.out.println(string2 + " is lexicographically smaller than " + string1);
-            else if(i1==string1.length() && i1!=string2.length())
+            else if(i==string1.length() && i!=string2.length())
                 System.out.println(string1 + " is lexicographically smaller than " + string2);
             else System.out.println(string2 + " and " + string1 + " are lexicographically equal");
         }
