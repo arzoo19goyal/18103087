90fee6151027c03056cfc5da73eaee8c1c7a1f9c
arzoo19goyal <56269320+arzoo19goyal@users.noreply.github.com>
2020-09-07 13:13:00 +0530

    Update Q4.java

--- Asignment2/Q4.java
+++ Asignment2/Q4.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int sum=0, x=1;
-        for(; x<Integer.MAX_VALUE; x++) {
+        int sum=0;
+        for(x=1; x<Integer.MAX_VALUE; x++) {
             if(sum >= Integer.MAX_VALUE - x) break;
             sum += x;
             if((int)Math.sqrt(sum)==x)
