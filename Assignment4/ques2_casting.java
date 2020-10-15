9e13360e4fb93dc979c0a9c4ff7a46b6bf876bf1
arzoo19goyal <56269320+arzoo19goyal@users.noreply.github.com>
2020-10-15 10:49:29 +0530

    Update ques2_casting.java

--- Assignment4/ques2_casting.java
+++ Assignment4/ques2_casting.java
@@ -16,9 +16,10 @@ class casting{
 
 /* explanation */
 /*
-int type casted to byte: byte datatype is 8 bit number of range -128 to +127. So, -1 can be easily stored.
+int type casted to byte: int datatype lies in range of -2147483648 to 2147483647 and byte datatype is 8 bit number of range -128 to +127.
+So, when we cast -1 to byte, it will remain same.
 
 byte type casted to char: The char datatype is 16 bit Unicode character. It has a minimum value of '\u0000' and a maximum of '\uffff'.
-
+When we cast a -ve number to char, the result will be same as subtracting that number from 65536.
 char type casted to int: Finally the char is type casted to integer value. int is of 32 bits and char is 16 bit. So, final integer = 2^16 - 1.
 */
