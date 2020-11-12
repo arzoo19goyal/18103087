623700423f85ac2abe02d69a13d1c41024fefbfd
arzoo19goyal <56269320+arzoo19goyal@users.noreply.github.com>
2020-11-12 23:57:41 +0530

    Update ques2.java

--- Assignment5/ques2.java
+++ Assignment5/ques2.java
@@ -1,121 +1,100 @@
-public class q2 extends Thread {
+import java.util.*;
 
-	public static long maxDiv = 0;
-	public static long num = 0;
-
-	public long start;
-
-	public q2(long s) {
-		this.start = s;
-	}
-
-	public void run() {
-		for(long i = this.start; i<=this.start+10000; i++) {
-			long currDiv=0;
-            for(long j=i; j>0; j--)
-            {
-                if(i%j==0)
-                {
-                    currDiv+=1;
-                }
+public class ques2 {
+   private final static int MAX = 100000;
+   private volatile static int maxDivisorcnt = 0;
+   private volatile static int intWithMaxDivisorcnt;
+   synchronized private static void report(int maxcntFromThread, 
+         int intWithMaxFromThread) {
+      if (maxDivisorcnt < maxcntFromThread) {
+         intWithMaxDivisorcnt = intWithMaxFromThread;
+	 maxDivisorcnt = maxcntFromThread;
+         
+      }
+   }
+   private static class cntDivisorsThread extends Thread {
+      int min, max;
+      public cntDivisorsThread(int min, int max) {
+         this.min = min;
+         this.max = max;
+      }
+      public void run() {
+         int maxDivisors = 0;
+         int whichInt = 0;
+         for (int i = min; i < max; i++) {
+            int divisors = cntDivisors(i);
+            if (divisors > maxDivisors) {
+               maxDivisors = divisors;
+               whichInt = i;
             }
-            if(currDiv > maxDiv)
-            {
-            	synchronized(this) {
-            		maxDiv = currDiv;
-	                num = i;
-            	}
+         }
+         report(maxDivisors,whichInt);
+      }
+   }
+	
+   public static int cntDivisors(int n) {
+      int cnt = 0;
+      int i = 1;
+      while (i <= n) {
+	      i++;
+         if ( n % i == 0 )
+            cnt ++;
+      }
+      return cnt;
+   }
+	
+   private static void cntDivisorsWithThreads(int numberOfThreads) {
+      System.out.println("\ncounting divisors using " + 
+            numberOfThreads + " threads...");
+	
+      cntDivisorsThread[] worker = new cntDivisorsThread[numberOfThreads];
+      long startTime = System.currentTimeMillis();
+      int integersPerThread = MAX/numberOfThreads; 
+      int start = 1;  
+      int end = start + integersPerThread - 1;  
+      int i = 0;   
+      while (i < numberOfThreads) {
+	 i++;
+         if (i == numberOfThreads - 1) {
+            end = MAX;  
+         }
+         worker[i] = new cntDivisorsThread( start, end );
+         start = end+1;    
+         end = start + integersPerThread - 1;
+      }
+      maxDivisorcnt = 0;
+      for (int i = 0; i < numberOfThreads; i++)
+         worker[i].start();
+      for (int i = 0; i < numberOfThreads; i++) {
+         while (worker[i].isAlive()) {
+            try {
+               worker[i].join();
             }
-		}
-	}
-
-	public static void main(String[] args) {
-		q2 th1 = new q2(1);
-		q2 th2 = new q2(10001);
-		q2 th3 = new q2(20001);
-		q2 th4 = new q2(30001);
-		q2 th5 = new q2(40001);
-		q2 th6 = new q2(50001);
-		q2 th7 = new q2(60001);
-		q2 th8 = new q2(70001);
-		q2 th9 = new q2(80001);
-		q2 th10 = new q2(90001);
-
-		long st = System.currentTimeMillis();
-
-		th1.start();
-		try {
-			th1.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		th2.start();
-		try {
-			th2.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		th3.start();
-		try {
-			th3.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		th4.start();
-		try {
-			th4.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		th5.start();
-		try {
-			th5.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		th6.start();
-		try {
-			th6.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		th7.start();
-		try {
-			th7.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		th8.start();
-		try {
-			th8.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		th9.start();
-		try {
-			th9.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		th10.start();
-		try {
-			th10.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		long total_time = System.currentTimeMillis() - st;
-
-		System.out.println("Number =  " + num + " Divisors = " + maxDiv);
-        System.out.println("Total time elapsed in milliseconds: " + (total_time));
-	}
+            catch (InterruptedException e) {
+            }
+         }
+      }
+      long elapsedTime = System.currentTimeMillis() - startTime;
+      System.out.println("\nThe largest number of divisors " + 
+            "for numbers between 1 and " + MAX + " is " + maxDivisorcnt);
+      System.out.println("An integer with that many divisors is " + 
+            intWithMaxDivisorcnt);
+      System.out.println("Total elapsed time:  " + 
+            (elapsedTime/1000.0) + " seconds.\n");
+   }
+   
+ 
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      int numberOfThreads = 0;
+      while (numberOfThreads > 10 || numberOfThreads < 1) {
+         System.out.print("How many threads do you want to use  (1 to 10) ?  ");
+         numberOfThreads = in.nextInt();
+         if (numberOfThreads > 10 || numberOfThreads < 1)
+            System.out.println("Enter a number from 1 to 10 !");
+      }
+      cntDivisorsWithThreads(numberOfThreads);
+   }
+   
+   
 }
