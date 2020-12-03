bab3eec77312497bc2a541c405f9025104ef592a
arzoo19goyal <56269320+arzoo19goyal@users.noreply.github.com>
2020-12-04 00:41:44 +0530

    Update brackets.js

--- Assignment5/brackets.js
+++ Assignment5/brackets.js
@@ -1,41 +1,51 @@
-const openers = ["{", "[", "("];
-const closers = ["}", "]", ")"];
-const match = {
-  "}": "{",
-  "]": "[",
-  ")": "("
-};
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form >
+        <input type="text" id="input_id" style="font-size:30px" value="">
+        <input type="button" value="Submit" style="font-size:30px" onclick="balance()" />
+        <div id="alert" style="color: red; font-size:50px" ></div>
+    </form>
+    <script src="brackets.js">
+      function balance() {
+        var variable = document.getElementById('input_id').value;
 
-function myFunction(string) {
-  console.log(`"${string}"`);
-  var stack = [];
+        const openers = ["{", "[", "("];
+        const closers = ["}", "]", ")"];
+        const match = {
+          "}": "{",
+          "]": "[",
+          ")": "("
+        };
 
-  for (c in string) {
-    if (openers.includes(string[c])) {
-      stack.push(string[c]);
-    }
-    if (closers.includes(string[c])) {
-      const last = stack.pop();
-      if (last !== match[string[c]]) {
-        document.write(string);
-        return false;
+        function myFunction(string) {
+          // console.log(`"${string}"`);
+          var stack = [];
+
+          for (c in string) {
+            if (openers.includes(string[c])) {
+              stack.push(string[c]);
+            }
+            if (closers.includes(string[c])) {
+              const last = stack.pop();
+              if (last !== match[string[c]]) {
+                return false;
+              }
+            }
+//             else{
+//               alert("Invalid input");
+//             }
+          }
+          return stack.length === 0; 
+        }
+
+        document.getElementById('alert').innerHTML = myFunction(variable);
       }
-    }
-  }
-  document.write(string);
-  return stack.length === 0; 
-}
-
-function test(actual, expected) {
-  document.write("&nbsp&nbsp", "actual:", actual,"&nbsp&nbsp", ",expected:", expected);
-  document.write("<br>");
-}
-test(myFunction(""), true);
-test(myFunction("{{{}}}]"), false);
-test(myFunction("{"), false);
-test(myFunction("}"), false);
-test(myFunction("{ [ ] ( { [ ] } ) }"), true);
-test(myFunction("{ [ () ] ( ) }"), true);
-test(myFunction("{ [ ( ] ) ( ) }"), false);
-test(myFunction("{ [ ( ] ) }"), false);
-test(myFunction("{ [ }"), false);
+      </script>
+</body>
+</html>
